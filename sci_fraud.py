6690af8e112fca2dd9fd97f7b492d95d07e40d16
Brooks Wood <bwood@fwrdthinking.com>
2019-09-23 11:20:21 -0600

    initial commit

---
+++ sci_fraud.py
@@ -0,0 +1,18 @@
+# To add a new cell, type '#%%'
+#%%
+import pandas as pd  
+import numpy as np  
+import matplotlib.pyplot as plt  
+import seaborn as seabornInstance 
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+
+dataset = pd.read_csv('Weather.csv')
+%matplotlib inline
+
+# To add a new markdown cell, type '#%% [markdown]'
+
+
+
+#%%
